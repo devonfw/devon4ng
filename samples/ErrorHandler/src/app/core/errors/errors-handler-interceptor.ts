63f62c9f879150cf08d38f5695e2cf76c36d615d
Cristian <cristian.trives@capgemini.com>
2019-03-06 10:21:08 +0100

    error-handler sample updated new handler interceptor

--- samples/ErrorHandler/src/app/core/errors/errors-handler-interceptor.ts
+++ samples/ErrorHandler/src/app/core/errors/errors-handler-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { retryWhen, delay, take, concatMap } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorsHandlerInterceptor implements HttpInterceptor {
@@ -9,7 +9,16 @@ export class ErrorsHandlerInterceptor implements HttpInterceptor {
     constructor() {}
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
-            retry(5)
+            retryWhen((errors: Observable<any>) => errors.pipe(
+                delay(500),
+                take(5),
+                concatMap((error: any, retryIndex: number) => {
+                    if (++retryIndex === 5) {
+                        throw error;
+                    }
+                    return of(error);
+                })
+            ))
         );
     }
 }

82b7ffc07a52fa6307c4a7649efd34f32ac9c889
Santos Jiménez Linares <santos.jimenez-linares@capgemini.com>
2020-02-25 13:35:57 +0100

    ErrorHandler sample app imports optimized

--- samples/ErrorHandler/src/app/app.module.ts
+++ samples/ErrorHandler/src/app/app.module.ts
@@ -1,12 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorsHandlerModule } from './core/errors/errors-handler.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],

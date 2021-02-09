553383ac7d4bc1604dc4a88b2040998104055a7d
Santos Jiménez Linares <sjimenez77@gmail.com>
2021-02-09 09:57:44 +0100

    AngularLazyLoading sample updated to Angular 11 and ESLint

--- samples/AngularLazyLoading/src/app/first/first/first.component.spec.ts
+++ samples/AngularLazyLoading/src/app/first/first/first.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FirstComponent } from './first.component';
 
@@ -6,7 +6,7 @@ describe('FirstComponent', () => {
   let component: FirstComponent;
   let fixture: ComponentFixture<FirstComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FirstComponent ]
     })

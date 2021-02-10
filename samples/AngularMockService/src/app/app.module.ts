3f88c1286394b12f64d7e9be56a6d033881aa08e
Santos Jiménez Linares <sjimenez77@gmail.com>
2021-02-10 10:24:05 +0100

    AngularMockService sample updated to Angular 11 and ESLint

--- samples/AngularMockService/src/app/app.module.ts
+++ samples/AngularMockService/src/app/app.module.ts
@@ -1,52 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { DataComponent } from './pages/data/data.component';
 import { DataTableComponent } from './components/data-table/data-table.component';
-import { FavoriteElementsComponent } from './components/favorite-elements/favorite-elements.component';
 import { ElementDetailsComponent } from './components/element-details/element-details.component';
+import { FavoriteElementsComponent } from './components/favorite-elements/favorite-elements.component';
+import { DataComponent } from './pages/data/data.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DataComponent, DataTableComponent, FavoriteElementsComponent, ElementDetailsComponent, DetailsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DataComponent,
+    DataTableComponent,
+    FavoriteElementsComponent,
+    ElementDetailsComponent,
+    DetailsComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- samples/AngularMockService/src/app/services/chemical-elements.service.ts
+++ samples/AngularMockService/src/app/services/chemical-elements.service.ts
@@ -1,27 +1,24 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 // import the model (interface) which allows to cast it's type to the service properties
-import {PeriodicElement} from '../models/periodicElement';
+import { PeriodicElement } from '../models/periodicElement';
 
-// import the mock data (will be replaced in the future) 
-import {ELEMENT_DATA} from '../mocks/elementData';
-
+// import the mock data (will be replaced in the future)
+import { ELEMENT_DATA } from '../mocks/elementData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ChemicalElementsService {
-
-  constructor() { }
+  constructor() {}
 
   public getElements(): Observable<PeriodicElement[]> {
     return of(ELEMENT_DATA);
   }
 
   public getElementAtPosition(pos: number): Observable<PeriodicElement> {
-    return of(ELEMENT_DATA.find(element => element.position === pos));
+    return of(ELEMENT_DATA.find((element) => element.position === pos));
   }
 
   public getFavoriteElements(): Observable<PeriodicElement[]> {
